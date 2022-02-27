64b9c8ce07d6e53bf7bf201cc53adb0834fe4e68
Kristina <kristina.diamond@kryoinc.com>
2022-02-26 21:42:28 -0500

    set up form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form v-if="showTipForm">
+      <label>Send me Eth!</label>
+      <input v-model="ethValue">
+      <button @click.prevent="submit($event)">Submit</button>
+    </form>
+    <div v-else>Please install an Eth wallet (ex: https://metamask.io/)</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Web3 from 'web3';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data(){
+    return {
+      web3: new Web3(Web3.givenProvider),
+      showTipForm: window.ethereum ? true : false,
+      ethValue: "0.01",
+    }
+  },
+  mounted() {},
+  computed: {
+    
+  },
+  methods: {
+    async send(amount) {
+      // get a list of accounts from metamask
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+
+      // turn the amount into the base unit of currency, wei
+      const wei = this.web3.utils.toWei(amount, "ether")
+      
+      // if at least one account
+      if (accounts.length > 0) {
+        // set up a transaction from logged in account
+        // to a set amount, with the value in hex format
+        window.ethereum.request({ 
+          method: "eth_sendTransaction",
+          params: [{
+            from: accounts[0],
+            to: "0x039C46B3bDd3cE8F7FdE0BaaBA1b3295E56ba85d",
+            value: this.web3.utils.toHex(wei)
+          }]
+        })
+      }
+    },
+    submit() {
+      // if a wallet installed
+      if (window.ethereum) {
+        // get the input and pass to send function
+        this.send(this.ethValue)
+      }
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+
+
+import './assets/base.scss';
+import './assets/style.scss';
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
